225b9da9747952cd1ff0e7d3c834082f7303b577
morgannegagne <morgannegagne@gmail.com>
2018-03-19 10:02:49 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  let actorsList= actors.map(a => (
+    <div>
+      <h2> {a.name}</h2>
+      <p>Movies: </p>
+      <ul>{a.movies.map(m => <li>{m}</li>)}</ul>
+    </div>
+  ))
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsList}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Home from './Home'
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <div className="navbar">
+      <ul>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/movies'>Movies</NavLink></li>
+        <li><NavLink to='/directors'>Directors</NavLink></li>
+        <li><NavLink to='/actors'>Actors</NavLink></li>
+    </ul>
+  </div>
   );
 };
 
