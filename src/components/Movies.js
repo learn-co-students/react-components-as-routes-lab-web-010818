09cf9ef9f0ca4c18f275c41ded1101ce19e9e0dc
cnlucke <cristy.lucke@gmail.com>
2018-03-19 12:15:38 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const movieDivs = movies.map((movie) => {
+    return (
+      <div>
+        <h1>{movie.title}</h1>
+        <p>{`${movie.time} minutes`}</p>
+        <ul>{movie.genres.map((genre) => <li>{genre}</li>)}</ul>
+      </div>
+    )
+  })
   return (
     <div>
-        {/*{code here}*/}
+        <h1>{'Movies Page'}</h1>
+        {movieDivs}
     </div>
   );
 };
 
 export default Movies;
+
+
+//make a new <div> for each movie. The <div> should contain the movie's title, time and an <ul> for each genre.
