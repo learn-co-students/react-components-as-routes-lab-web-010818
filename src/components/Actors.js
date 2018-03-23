b53aba7eef6fe889722d791162d612666ac56c69
Fei <fei.hafferkamp@gmail.com>
2018-03-22 22:24:04 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors);
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((a, i) => (
+        <div key={i}>
+          <h3>{a.name}</h3>
+          <ul>{a.movies.map((m, i) => <li key={i}>{m}</li>)}</ul>
+        </div>
+      ))}
     </div>
   );
 };
